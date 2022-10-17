fe685b941ec8331052155129e1a2b91946321517
zyperr <ramerica200@gmail.com>
2022-10-17 05:00:23 -0300

    Enviando proyecto final

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+import {addTask} from "/components/addTask.js";
+
+const btn = document.querySelector("[data-form-btn]");
+
+//Arrow functions o funciones anonimas
+btn.addEventListener("click", addTask);
+
+
+const darkmode = document.querySelector(".darkmode");
+
+const cardContainer = document.querySelector(".mainCard");
+darkmode.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  cardContainer.classList.toggle("cardModoOscuro");
+});
