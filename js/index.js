71b4616fe1ec9b3134c025f4f3b40ce6e46f79bb
zyperr <ramerica200@gmail.com>
2022-10-17 06:23:13 -0300

    agregando llamada a funcion faltante

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import { addTask } from "../components/addTask.js";
-
+import { displayTasks } from "../components/readTasks.js";
 const btn = document.querySelector("[data-form-btn]");
 
 //Arrow functions o funciones anonimas
 btn.addEventListener("click", addTask);
+
+displayTasks();
 
 const darkmode = document.querySelector(".darkmode");
 
